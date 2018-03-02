3efa73aa2fee190b3875e2d7fa9e0d2a80f4b7cc
Julian Johannesen <julian.johannesen@gmail.com>
2018-03-02 18:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 function appendDriver(name){
-  var newDrivers = drivers.slice();
-  newDrivers.push(name);
+
 }
 function prependDriver(name){
 
 }
 function removeLastDriver(){
-  var newArray = drivers.slice(-1,1);
+  var newArray = drivers.slice(0,-1);
   return newArray;
 }
 function removeFirstDriver(){
-  var newArray = drivers.slice(0,1);
+  var newArray = drivers.slice(1);
   return newArray;
 }
